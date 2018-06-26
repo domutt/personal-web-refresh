08d37806d71075da6fc22c4b201a7625dd9aff49
Dominic Muttel <dmac@Dominics-MacBook-Air.local>
2018-06-26 11:21:10 -0600

    fixed z-index bug

--- prod/app.js
+++ prod/app.js
@@ -4,39 +4,54 @@ $(document).ready( function() {
     $("#menu-container").attr("style", "display:none;");
 
     $( "#left-button" ).click(function() {
-        $("#left-button").css({"box-shadow" : "2px 1.5px #888988"});
-        $("#right-button").removeAttr("style");
-        $("#gd-menu").animate({"width": "50%"}, 2000);
-        $("#ui-menu").animate({"width": "50%"}, 2000);
-        $("#menu-container").fadeOut("fast");
-        $("#design-portfolio").fadeOut("fast");
-        $("#ui-portfolio").fadeOut("fast");
+        $( "#left-button" ).css({"box-shadow" : "2px 1.5px #888988"});
+        $( "#right-button" ).removeAttr("style");
+        $("#menu-container").hide();
+        $("#design-portfolio").hide();
+        $("#ui-portfolio").hide();
+        $("#gd-menu").delay(1000).animate({"width": "50%"});
+        $("#ui-menu").delay(1000).animate({"width": "50%"});
         $("#about-container").fadeIn("slow");
-        $("#small-logo").fadeOut("fast");
+        $("#small-logo").fadeOut();
     });
-    $("#right-button").click(function() {
-        $("#about-container").fadeOut("fast");
+
+    $( "#right-button" ).click(function() {
+        $("#about-container").hide();
         $("#small-logo").fadeIn();
-        $("#right-button").css({"box-shadow" : "-2px 1.5px #888988"});
-        $("#left-button").removeAttr("style");
-        $("#menu-container" ).fadeIn();
-        $("#gd-menu").animate({"width": "50%", "z-index": "999"},500);
+        $( "#right-button" ).css({"box-shadow" : "-2px 1.5px #888988"});
+        $( "#left-button" ).removeAttr("style");
+        $( "#menu-container" ).fadeIn();
+        $("#gd-menu").animate({"width": "50%", "z-index": "999"},500 );
         $("#ui-menu").animate({"width": "50%" , "z-index": "999"}, 500);
         $("#design-portfolio").hide();
         $("#ui-portfolio").hide();
         $("#ui-menu").removeAttr("style");
         $("#gd-menu").removeAttr("style");
     });
+    // $("#gd-menu").css({"border": "4px #FFDEFD solid"});
+    // $("#gd-menu").hover(function () {
+    //     $("#gd-outline").css({"border": "4px #FFDEFD solid"});
+    // });
     $("#gd-menu").click(function () {
+        // $("#gd-outline").css({"border": "4px #FFFFFF solid"});
         $("#ui-menu").animate({"width": "0%", "z-index": "0"}, 500).removeAttr();
-        $("#gd-menu").animate({"width": "100%", "z-index": "999"}, 500).attr("style", "background-image: url('../images/about-bg.png'); background-color:  #FFDEFD; color: #FFFFFF; background-size: 1440px; background-position: right; background-repeat: no-repeat; background-blend-mode: multiply;");
-        $("#design-portfolio").delay(480).fadeIn("slow");
-        $("#small-logo").fadeOut("fast");
+        $("#gd-menu").animate({"width": "100%", "z-index": "999"}, 500).attr("style", "background-image: url('../images/about-bg.png'); background-color:  #FFDEFD; color: #FFFFFF; background-size: cover; background-position: right; background-repeat: no-repeat; background-blend-mode: multiply;");
+        $("#design-portfolio").delay(480).fadeIn("fast");
+        $("#small-logo").fadeOut();
     });
+    // $("#ui-menu").css({"border": "4px #FFFACC solid"});
+    // $("#ui-menu").mouseOver(function () {
+    //     $("#ui-outline").css({"border": "4px #FFFFFF solid"});
+    // });
+    // $("#ui-menu").mouseLeave(function () {
+    //     $("#ui-outline").css({"border": "4px #FFFACC solid"});
+    // });
     $("#ui-menu").click(function () {
         $("#gd-menu").animate({"width": "0%", "z-index": "0"}, 500).removeAttr();
-        $("#ui-menu").animate({"width": "100%", "z-index": "999"}, 500).attr("style", "background-image: url('../images/about-bg-reverse.png'); background-color:  #FFFACC; color: #FFFFFF; background-size: 1440px; background-position: left; background-repeat: no-repeat; background-blend-mode: multiply;");
-        $("#ui-portfolio").delay(480).fadeIn("slow");
-        $("#small-logo").fadeOut("fast");
+        $("#ui-menu").animate({"width": "100%", "z-index": "999"}, 500).attr("style", "background-image: url('../images/about-bg.png'); background-color:  #FFFACC; color: #FFFFFF; background-size: cover; background-position: left; background-repeat: no-repeat; background-blend-mode: multiply;");
+        $("#ui-portfolio").delay(480).fadeIn("fast");
+        $("#small-logo").fadeOut();
     });
+    $('.portfolio-header').parallax({imageSrc: '../images/gd_projects/3people-book.png'});
+
 });
